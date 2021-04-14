e3cfda50a833e46d37b8e0dba6e973aaaf8cb92a
marsislav <mkostadinov87@gmail.com>
2021-04-14 20:49:35 +0300

    mCMS 2.1 (Pagination)

--- index.php
+++ index.php
@@ -9,7 +9,32 @@ include "includes/header.php";
       <div class="row">
       <div class="col-md-8">
         <!-- Blog Entries Column -->
-        <?php $query = "SELECT * FROM posts"; 
+        
+        <?php 
+        $posts_per_page=5;
+        /*Pagination */
+        if (isset($_GET['page'])) {
+          $page=$_GET['page'];
+        }
+        else {
+          $page="";
+        }
+        if ($page=="" || $page==1) {
+          $page_1=0;
+
+        }
+        else {
+          $page_1=($page * $posts_per_page)-$posts_per_page;
+        }
+        $post_query_count="SELECT * FROM posts";
+        $find_count=mysqli_query($connection, $post_query_count);
+        $count=mysqli_num_rows($find_count);
+        $count=ceil($count/$posts_per_page);
+
+        
+        /*Pagination */
+        
+        $query = "SELECT * FROM posts LIMIT $page_1, $posts_per_page"; 
         $select_all_posts_query=mysqli_query($connection, $query);
         while ($row=mysqli_fetch_assoc($select_all_posts_query)) {
           
@@ -75,4 +100,15 @@ include "includes/header.php";
 
       <hr />
     </div>
+    <ul class="pager">
+    <?php 
+    for ($i=1; $i<=$count; $i++) {
+      if ($i==$page) {
+        echo "<li><a href='index.php?page={$i}' class='active_link'>{$i}</a></li>";
+      }
+      else {
+      echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+    }}
+    ?>
+    </ul>
     <?php include "includes/footer.php";?>
